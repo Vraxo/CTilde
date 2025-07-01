78dd5fe6583812f5821ff3b3cc7f85118241c7d3
Parsa <pkafsharix@gmail.com>
2025-07-01 20:02:47 +0330

    Add project files.

---
+++ CTilde/Source/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CTilde;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string code = File.ReadAllText("main.c");
+
+        Tokenizer.Tokenize(code);
+    }
+}
---
+++ CTilde/Source/Tokenizer.cs
@@ -0,0 +1,107 @@
+﻿namespace CTilde;
+
+public enum TokenType
+{
+    Keyword,
+    Identifier,
+    IntegerLiteral,
+    Semicolon,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Unknown
+}
+
+public record Token(TokenType Type, string Value);
+
+public class Tokenizer
+{
+    private static readonly HashSet<string> Keywords =
+    [
+        "int",
+        "return"
+    ];
+
+    public static void Tokenize(string input)
+    {
+        List<Token> tokens = [];
+        int i = 0;
+
+        while (i < input.Length)
+        {
+            char c = input[i];
+
+            if (char.IsWhiteSpace(c))
+            {
+                i++;
+                continue;
+            }
+
+            switch (c)
+            {
+                case '(':
+                    tokens.Add(new(TokenType.LeftParen, "("));
+                    i++;
+                    continue;
+                case ')':
+                    tokens.Add(new(TokenType.RightParen, ")"));
+                    i++;
+                    continue;
+                case '{':
+                    tokens.Add(new(TokenType.LeftBrace, "{"));
+                    i++;
+                    continue;
+                case '}':
+                    tokens.Add(new(TokenType.RightBrace, "}"));
+                    i++;
+                    continue;
+                case ';':
+                    tokens.Add(new(TokenType.Semicolon, ";"));
+                    i++;
+                    continue;
+            }
+
+            if (char.IsLetter(c))
+            {
+                int start = i;
+                
+                while (i < input.Length && char.IsLetterOrDigit(input[i]))
+                {
+                    i++;
+                }
+
+                string value = input[start..i];
+
+                TokenType type = Keywords.Contains(value) 
+                    ? TokenType.Keyword 
+                    : TokenType.Identifier;
+
+                tokens.Add(new(type, value));
+                continue;
+            }
+
+            if (char.IsDigit(c))
+            {
+                int start = i;
+
+                while (i < input.Length && char.IsDigit(input[i]))
+                {
+                    i++;
+                }
+
+                string value = input[start..i];
+                tokens.Add(new(TokenType.IntegerLiteral, value));
+                continue;
+            }
+
+            tokens.Add(new(TokenType.Unknown, c.ToString()));
+            i++;
+        }
+
+        foreach (Token token in tokens)
+        {
+            Console.WriteLine($"Type: {token.Type,-15} Value: '{token.Value}'");
+        }
+    }
+}

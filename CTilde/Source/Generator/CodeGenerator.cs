796ce543890f941f7e5f1e07ad04a555e399bf96
Parsa <pkafsharix@gmail.com>
2025-07-03 14:45:38 +0330

    DeclarationGenerator

--- CTilde/Source/Generator/CodeGenerator.cs
+++ CTilde/Source/Generator/CodeGenerator.cs
@@ -16,8 +16,9 @@ public class CodeGenerator
     private readonly Dictionary<string, string> _stringLiterals = new();
     internal HashSet<string> ExternalFunctions { get; } = new();
 
-    private readonly StatementGenerator _statementGenerator;
+    internal StatementGenerator StatementGenerator { get; }
     internal ExpressionGenerator ExpressionGenerator { get; }
+    private readonly DeclarationGenerator _declarationGenerator;
 
     public CodeGenerator(ProgramNode program)
     {
@@ -27,7 +28,8 @@ public class CodeGenerator
         SemanticAnalyzer = new SemanticAnalyzer(TypeManager, MemoryLayoutManager);
 
         ExpressionGenerator = new ExpressionGenerator(this);
-        _statementGenerator = new StatementGenerator(this);
+        StatementGenerator = new StatementGenerator(this);
+        _declarationGenerator = new DeclarationGenerator(this);
     }
 
     public string Generate()
@@ -53,7 +55,7 @@ public class CodeGenerator
         {
             foreach (var function in unit.Functions.Where(f => f.Body != null))
             {
-                GenerateFunction(function, unit, null);
+                _declarationGenerator.GenerateFunction(function, unit, null);
                 Builder.AppendBlankLine();
             }
 
@@ -61,17 +63,17 @@ public class CodeGenerator
             {
                 foreach (var method in s.Methods.Where(m => m.Body != null))
                 {
-                    GenerateFunction(method, unit, s);
+                    _declarationGenerator.GenerateFunction(method, unit, s);
                     Builder.AppendBlankLine();
                 }
                 foreach (var ctor in s.Constructors)
                 {
-                    GenerateConstructor(ctor, unit);
+                    _declarationGenerator.GenerateConstructor(ctor, unit);
                     Builder.AppendBlankLine();
                 }
                 foreach (var dtor in s.Destructors)
                 {
-                    GenerateDestructor(dtor, unit);
+                    _declarationGenerator.GenerateDestructor(dtor, unit);
                     Builder.AppendBlankLine();
                 }
             }
@@ -129,138 +131,5 @@ public class CodeGenerator
                 foreach (var c in children) FindAllStringLiterals(c);
             }
         }
-    }
-
-    private void GenerateConstructor(ConstructorDeclarationNode ctor, CompilationUnitNode unit)
-    {
-        var symbols = new SymbolTable(ctor, TypeManager, MemoryLayoutManager, unit);
-        // Create a dummy function node to provide context for analysis, preventing NullReferenceException.
-        var dummyFunctionForContext = new FunctionDeclarationNode(
-            new Token(TokenType.Keyword, "void"), 0, ctor.OwnerStructName,
-            ctor.Parameters, ctor.Body, ctor.OwnerStructName, ctor.AccessLevel,
-            false, false, ctor.Namespace
-        );
-        var context = new AnalysisContext(symbols, unit, dummyFunctionForContext);
-
-        Builder.AppendLabel(NameMangler.Mangle(ctor));
-        GeneratePrologue(symbols);
-
-        if (ctor.Initializer != null)
-        {
-            var ownerStruct = TypeManager.FindStructByUnqualifiedName(ctor.OwnerStructName, ctor.Namespace) ?? throw new InvalidOperationException("Owner struct not found");
-
-            var argTypes = ctor.Initializer.Arguments
-                .Select(arg => SemanticAnalyzer.AnalyzeExpressionType(arg, context))
-                .ToList();
-            var baseFqn = TypeManager.ResolveTypeName(ownerStruct.BaseStructName!, ownerStruct.Namespace, unit);
-            var baseCtor = TypeManager.FindConstructor(baseFqn, argTypes) ?? throw new InvalidOperationException("Base constructor not found for given argument types.");
-
-
-            int totalArgSize = 0;
-            foreach (var arg in ctor.Initializer.Arguments.AsEnumerable().Reverse())
-            {
-                totalArgSize += ExpressionGenerator.PushArgument(arg, context);
-            }
-
-            context.Symbols.TryGetSymbol("this", out var thisOffset, out _, out _);
-            Builder.AppendInstruction($"mov eax, [ebp + {thisOffset}]", "Get 'this' pointer");
-            Builder.AppendInstruction("push eax", "Push 'this' for base ctor");
-            totalArgSize += 4;
-
-            Builder.AppendInstruction($"call {NameMangler.Mangle(baseCtor)}");
-            Builder.AppendInstruction($"add esp, {totalArgSize}", "Clean up base ctor args");
-            Builder.AppendBlankLine();
-        }
-
-        _statementGenerator.GenerateStatement(ctor.Body, context);
-        GenerateEpilogue(new List<(string, int, string)>());
-    }
-
-    private void GenerateDestructor(DestructorDeclarationNode dtor, CompilationUnitNode unit)
-    {
-        var symbols = new SymbolTable(dtor, TypeManager, MemoryLayoutManager, unit);
-        // Create a dummy function node to provide context for analysis.
-        var dummyFunctionForContext = new FunctionDeclarationNode(
-            new Token(TokenType.Keyword, "void"), 0, dtor.OwnerStructName,
-            new List<ParameterNode>(), dtor.Body, dtor.OwnerStructName, dtor.AccessLevel,
-            dtor.IsVirtual, false, dtor.Namespace
-        );
-        var context = new AnalysisContext(symbols, unit, dummyFunctionForContext);
-
-        Builder.AppendLabel(NameMangler.Mangle(dtor));
-        GeneratePrologue(symbols);
-        _statementGenerator.GenerateStatement(dtor.Body, context);
-        GenerateEpilogue(new List<(string, int, string)>());
-    }
-
-    private void GenerateFunction(FunctionDeclarationNode function, CompilationUnitNode unit, StructDefinitionNode? owner)
-    {
-        var symbols = new SymbolTable(function, TypeManager, MemoryLayoutManager, unit);
-        var context = new AnalysisContext(symbols, unit, function);
-        var destructibleLocals = symbols.GetDestructibleLocals(TypeManager);
-
-        string mangledName = function.Name == "main" ? "_main" : NameMangler.Mangle(function);
-
-        Builder.AppendLabel(mangledName);
-        GeneratePrologue(symbols);
-
-        if (function.Body != null) _statementGenerator.GenerateStatement(function.Body, context);
-
-        GenerateEpilogue(destructibleLocals);
-    }
-
-    private void GeneratePrologue(SymbolTable symbols)
-    {
-        Builder.AppendInstruction("push ebp");
-        Builder.AppendInstruction("mov ebp, esp");
-        Builder.AppendInstruction("push ebx", "Preserve non-volatile registers");
-        Builder.AppendInstruction("push esi");
-        Builder.AppendInstruction("push edi");
-        Builder.AppendBlankLine();
-
-        int totalLocalSize = symbols.TotalLocalSize;
-        if (totalLocalSize > 0)
-        {
-            Builder.AppendInstruction($"sub esp, {totalLocalSize}", "Allocate space for all local variables");
-        }
-    }
-
-    private void GenerateEpilogue(List<(string Name, int Offset, string TypeFqn)> destructibleLocals)
-    {
-        if (destructibleLocals.Any())
-        {
-            Builder.AppendBlankLine();
-            Builder.AppendInstruction(null, "Destructor cleanup");
-            foreach (var (name, offset, type) in destructibleLocals.AsEnumerable().Reverse())
-            {
-                var dtor = TypeManager.FindDestructor(type);
-                if (dtor != null)
-                {
-                    Builder.AppendInstruction($"lea eax, [ebp + {offset}]", $"Get address of '{name}' for dtor");
-                    Builder.AppendInstruction("push eax");
-
-                    if (dtor.IsVirtual)
-                    {
-                        // The destructor is always at index 0 in the vtable if virtual
-                        Builder.AppendInstruction("mov eax, [eax]", "Get vtable ptr");
-                        Builder.AppendInstruction("mov eax, [eax]", "Get dtor from vtable[0]");
-                        Builder.AppendInstruction("call eax");
-                    }
-                    else
-                    {
-                        Builder.AppendInstruction($"call {NameMangler.Mangle(dtor)}");
-                    }
-                    Builder.AppendInstruction("add esp, 4", "Clean up 'this'");
-                }
-            }
-        }
-
-        Builder.AppendBlankLine();
-        Builder.AppendInstruction("pop edi");
-        Builder.AppendInstruction("pop esi");
-        Builder.AppendInstruction("pop ebx");
-        Builder.AppendInstruction("mov esp, ebp");
-        Builder.AppendInstruction("pop ebp");
-        Builder.AppendInstruction("ret");
     }
 }
---
+++ CTilde/Source/Generator/DeclarationGenerator.cs
@@ -0,0 +1,150 @@
+﻿namespace CTilde;
+
+public class DeclarationGenerator
+{
+    private readonly CodeGenerator _context;
+    private AssemblyBuilder Builder => _context.Builder;
+    private TypeManager TypeManager => _context.TypeManager;
+    private MemoryLayoutManager MemoryLayoutManager => _context.MemoryLayoutManager;
+    private SemanticAnalyzer SemanticAnalyzer => _context.SemanticAnalyzer;
+    private StatementGenerator StatementGenerator => _context.StatementGenerator;
+    private ExpressionGenerator ExpressionGenerator => _context.ExpressionGenerator;
+
+    public DeclarationGenerator(CodeGenerator context)
+    {
+        _context = context;
+    }
+
+    public void GenerateConstructor(ConstructorDeclarationNode ctor, CompilationUnitNode unit)
+    {
+        var symbols = new SymbolTable(ctor, TypeManager, MemoryLayoutManager, unit);
+        // Create a dummy function node to provide context for analysis, preventing NullReferenceException.
+        var dummyFunctionForContext = new FunctionDeclarationNode(
+            new Token(TokenType.Keyword, "void"), 0, ctor.OwnerStructName,
+            ctor.Parameters, ctor.Body, ctor.OwnerStructName, ctor.AccessLevel,
+            false, false, ctor.Namespace
+        );
+        var context = new AnalysisContext(symbols, unit, dummyFunctionForContext);
+
+        Builder.AppendLabel(NameMangler.Mangle(ctor));
+        GeneratePrologue(symbols);
+
+        if (ctor.Initializer != null)
+        {
+            var ownerStruct = TypeManager.FindStructByUnqualifiedName(ctor.OwnerStructName, ctor.Namespace) ?? throw new InvalidOperationException("Owner struct not found");
+
+            var argTypes = ctor.Initializer.Arguments
+                .Select(arg => SemanticAnalyzer.AnalyzeExpressionType(arg, context))
+                .ToList();
+            var baseFqn = TypeManager.ResolveTypeName(ownerStruct.BaseStructName!, ownerStruct.Namespace, unit);
+            var baseCtor = TypeManager.FindConstructor(baseFqn, argTypes) ?? throw new InvalidOperationException("Base constructor not found for given argument types.");
+
+
+            int totalArgSize = 0;
+            foreach (var arg in ctor.Initializer.Arguments.AsEnumerable().Reverse())
+            {
+                totalArgSize += ExpressionGenerator.PushArgument(arg, context);
+            }
+
+            context.Symbols.TryGetSymbol("this", out var thisOffset, out _, out _);
+            Builder.AppendInstruction($"mov eax, [ebp + {thisOffset}]", "Get 'this' pointer");
+            Builder.AppendInstruction("push eax", "Push 'this' for base ctor");
+            totalArgSize += 4;
+
+            Builder.AppendInstruction($"call {NameMangler.Mangle(baseCtor)}");
+            Builder.AppendInstruction($"add esp, {totalArgSize}", "Clean up base ctor args");
+            Builder.AppendBlankLine();
+        }
+
+        StatementGenerator.GenerateStatement(ctor.Body, context);
+        GenerateEpilogue(new List<(string, int, string)>());
+    }
+
+    public void GenerateDestructor(DestructorDeclarationNode dtor, CompilationUnitNode unit)
+    {
+        var symbols = new SymbolTable(dtor, TypeManager, MemoryLayoutManager, unit);
+        // Create a dummy function node to provide context for analysis.
+        var dummyFunctionForContext = new FunctionDeclarationNode(
+            new Token(TokenType.Keyword, "void"), 0, dtor.OwnerStructName,
+            new List<ParameterNode>(), dtor.Body, dtor.OwnerStructName, dtor.AccessLevel,
+            dtor.IsVirtual, false, dtor.Namespace
+        );
+        var context = new AnalysisContext(symbols, unit, dummyFunctionForContext);
+
+        Builder.AppendLabel(NameMangler.Mangle(dtor));
+        GeneratePrologue(symbols);
+        StatementGenerator.GenerateStatement(dtor.Body, context);
+        GenerateEpilogue(new List<(string, int, string)>());
+    }
+
+    public void GenerateFunction(FunctionDeclarationNode function, CompilationUnitNode unit, StructDefinitionNode? owner)
+    {
+        var symbols = new SymbolTable(function, TypeManager, MemoryLayoutManager, unit);
+        var context = new AnalysisContext(symbols, unit, function);
+        var destructibleLocals = symbols.GetDestructibleLocals(TypeManager);
+
+        string mangledName = function.Name == "main" ? "_main" : NameMangler.Mangle(function);
+
+        Builder.AppendLabel(mangledName);
+        GeneratePrologue(symbols);
+
+        if (function.Body != null) StatementGenerator.GenerateStatement(function.Body, context);
+
+        GenerateEpilogue(destructibleLocals);
+    }
+
+    private void GeneratePrologue(SymbolTable symbols)
+    {
+        Builder.AppendInstruction("push ebp");
+        Builder.AppendInstruction("mov ebp, esp");
+        Builder.AppendInstruction("push ebx", "Preserve non-volatile registers");
+        Builder.AppendInstruction("push esi");
+        Builder.AppendInstruction("push edi");
+        Builder.AppendBlankLine();
+
+        int totalLocalSize = symbols.TotalLocalSize;
+        if (totalLocalSize > 0)
+        {
+            Builder.AppendInstruction($"sub esp, {totalLocalSize}", "Allocate space for all local variables");
+        }
+    }
+
+    private void GenerateEpilogue(List<(string Name, int Offset, string TypeFqn)> destructibleLocals)
+    {
+        if (destructibleLocals.Any())
+        {
+            Builder.AppendBlankLine();
+            Builder.AppendInstruction(null, "Destructor cleanup");
+            foreach (var (name, offset, type) in destructibleLocals.AsEnumerable().Reverse())
+            {
+                var dtor = TypeManager.FindDestructor(type);
+                if (dtor != null)
+                {
+                    Builder.AppendInstruction($"lea eax, [ebp + {offset}]", $"Get address of '{name}' for dtor");
+                    Builder.AppendInstruction("push eax");
+
+                    if (dtor.IsVirtual)
+                    {
+                        // The destructor is always at index 0 in the vtable if virtual
+                        Builder.AppendInstruction("mov eax, [eax]", "Get vtable ptr");
+                        Builder.AppendInstruction("mov eax, [eax]", "Get dtor from vtable[0]");
+                        Builder.AppendInstruction("call eax");
+                    }
+                    else
+                    {
+                        Builder.AppendInstruction($"call {NameMangler.Mangle(dtor)}");
+                    }
+                    Builder.AppendInstruction("add esp, 4", "Clean up 'this'");
+                }
+            }
+        }
+
+        Builder.AppendBlankLine();
+        Builder.AppendInstruction("pop edi");
+        Builder.AppendInstruction("pop esi");
+        Builder.AppendInstruction("pop ebx");
+        Builder.AppendInstruction("mov esp, ebp");
+        Builder.AppendInstruction("pop ebp");
+        Builder.AppendInstruction("ret");
+    }
+}

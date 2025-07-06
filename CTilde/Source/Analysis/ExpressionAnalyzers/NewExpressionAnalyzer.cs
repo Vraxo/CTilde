142e9f4524b4ac4bc113122607ab2486b75fa823
Parsa <pkafsharix@gmail.com>
2025-07-06 22:14:50 +0330

    NewExpressionAnalyzer

--- CTilde/Source/Analysis/ExpressionAnalyzers/NewExpressionAnalyzer.cs
+++ CTilde/Source/Analysis/ExpressionAnalyzers/NewExpressionAnalyzer.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using CTilde.Diagnostics;
+﻿using CTilde.Diagnostics;
 
 namespace CTilde.Analysis.ExpressionAnalyzers;
 
@@ -18,33 +15,55 @@ public class NewExpressionAnalyzer : ExpressionAnalyzerBase
     public override string Analyze(ExpressionNode expr, AnalysisContext context, List<Diagnostic> diagnostics)
     {
         var n = (NewExpressionNode)expr;
+
+        string? typeName = ResolveAndValidateType(n, context, diagnostics);
+        
+        if (typeName is null)
+        {
+            return "unknown";
+        }
+
+        AnalyzeConstructorArguments(n, context, diagnostics);
+
+        return typeName + "*";
+    }
+
+    private string? ResolveAndValidateType(NewExpressionNode n, AnalysisContext context, List<Diagnostic> diagnostics)
+    {
         string typeName;
+
         try
         {
             typeName = _typeResolver.ResolveType(n.Type, context.CurrentFunction.Namespace, context.CompilationUnit);
         }
         catch (InvalidOperationException ex)
         {
-            var token = n.Type.GetFirstToken();
-            diagnostics.Add(new Diagnostic(context.CompilationUnit.FilePath, ex.Message, token.Line, token.Column));
-            typeName = "unknown";
+            Token token = n.Type.GetFirstToken();
+            diagnostics.Add(new(context.CompilationUnit.FilePath, ex.Message, token.Line, token.Column));
+            return null;
         }
 
-        // 'new' can only be used with struct types, not primitives like 'int'
         if (n.Type is SimpleTypeNode stn && stn.TypeToken.Type == TokenType.Keyword)
         {
-            var token = n.Type.GetFirstToken();
-            diagnostics.Add(new Diagnostic(context.CompilationUnit.FilePath, $"'new' cannot be used with primitive type '{stn.TypeToken.Value}'.", token.Line, token.Column));
-            return "unknown";
+            Token token = n.Type.GetFirstToken();
+
+            diagnostics.Add(new(
+                context.CompilationUnit.FilePath,
+                $"'new' cannot be used with primitive type '{stn.TypeToken.Value}'.",
+                token.Line,
+                token.Column));
+
+            return null;
         }
 
-        // Analyze constructor arguments to mark variables as used and check for other errors.
-        foreach (var arg in n.Arguments)
+        return typeName;
+    }
+
+    private void AnalyzeConstructorArguments(NewExpressionNode n, AnalysisContext context, List<Diagnostic> diagnostics)
+    {
+        foreach (ExpressionNode arg in n.Arguments)
         {
             _semanticAnalyzer.AnalyzeExpressionType(arg, context, diagnostics);
         }
-        // TODO: Add constructor resolution and signature matching check here
-
-        return typeName + "*";
     }
 }
